90eee041824078edb7c947f6a540db0498b4dde2
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-11-27 20:26:51 +0100

    #2290: helper to circumvent acquisition insanity

---
+++ browser/__init__.py
@@ -0,0 +1,48 @@
+# (C) Copyright 2010 Georges Racinet
+# Author: Georges Racinet <georges@racinet.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+
+from Products.Five.browser import BrowserView
+
+_missing_value = object()
+
+class AqSafeBrowserView(BrowserView):
+    """Base class protecting against some acquisition insanity (see #2290)
+    """
+
+    def __init__(self, *a, **k):
+        self.aq_safe = {}
+        return BrowserView.__init__(self, *a, **k)
+
+    def aqSafeGet(self, k, default=_missing_value):
+        if default is _missing_value:
+            try:
+                return self.aq_safe[k]
+            except KeyError:
+                raise AttributeError(k)
+
+        return self.aq_safe.get(k, default)
+
+    def aqSafeSet(self, k, v):
+        self.aq_safe[k] = v
+
+    def aqSafeDel(self, k):
+        try:
+            del self.aq_safe[k]
+        except KeyError:
+            raise AttributeError(k)
+
---
+++ tests/test_browser.py
@@ -0,0 +1,62 @@
+# (C) Copyright 2010 Georges Racinet
+# Author: Georges Racinet <georges@racinet.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+import os
+import unittest
+from Testing.ZopeTestCase import ZopeTestCase
+from zope.app.testing.functional import ZCMLLayer
+
+from zope.interface import Interface, implements
+from OFS.SimpleItem import SimpleItem
+from Products.CPSonFive.browser import AqSafeBrowserView
+
+class IContent(Interface):
+    pass
+
+class Content(SimpleItem):
+    implements(IContent)
+
+    def __init__(self, zid):
+        self._setId(zid)
+
+config_file = os.path.join(os.path.dirname(__file__), 'cpsonfive.zcml')
+
+CPSonFiveLayer = ZCMLLayer(config_file, __name__, 'CPSonFiveLayer')
+
+class AqSafeTestView(AqSafeBrowserView):
+
+    def meth(self):
+        return 'ok'
+
+class AqSafeBrowserViewTestCase(ZopeTestCase):
+    layer = CPSonFiveLayer
+
+    def afterSetUp(self):
+        self.folder._setOb('content', Content('content'))
+        self.view = self.folder.unrestrictedTraverse('content/aq_safe')
+
+    def test_set_get(self):
+        view = self.view
+        view.aqSafeSet('content', view.context)
+        content = view.aqSafeGet('content')
+        # breaks with normal attribute set/get
+        self.assertEquals(content.absolute_url_path(),
+                          '/test_folder_1_/content')
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(AqSafeBrowserViewTestCase)))
